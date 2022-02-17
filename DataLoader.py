55502b896c2a06d50413a896643e8008edbf4471
richard003 <414684261@qq.com>
2022-02-17 21:45:13 +0800

    infoGAN

--- DataLoader.py
+++ DataLoader.py
@@ -88,6 +88,7 @@ class Date(dataset.Dataset):
 transform_train = transforms.Compose([
     transforms.ToTensor(),
     transforms.Resize((256,256)),
+    transforms.Normalize([0.5], [0.5])
     # transforms.RandomResizedCrop(224,scale=(0.08,1.0),ratio=(3.0/4.0,4.0/3.0)),
     # transforms.RandomHorizontalFlip(),
     # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
@@ -97,6 +98,7 @@ transform_train = transforms.Compose([
 transform_val = transforms.Compose([
     transforms.ToTensor(),
     transforms.Resize((256,256)),
+    transforms.Normalize([0.5], [0.5])
     # transforms.CenterCrop(224),
     #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
